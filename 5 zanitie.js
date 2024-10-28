577469a586ac9d8cbfe544929ef3d7938dbe010e
Cadrol1213 <romanov.sh09@gmail.com>
2024-10-28 17:55:42 +0500

    изучали js XDDD

---
+++ 5 zanitie.js
@@ -0,0 +1,8 @@
+    let a = 1;
+    let hello = "Привет,";
+    let name = "САША";
+    let st = name + hello
+    console.log(st)
+    console.log(a)
+    let span = document.getElementById("answer")
+    span. innerText = 5;
