34acb21dd0305e0a03f5b42d82b09c5b1cf1d497
Cadrol1213 <romanov.sh09@gmail.com>
2024-12-18 19:59:39 +0500

    делаем задачиии 7

--- условия 6.js
+++ условия 6.js
@@ -3,9 +3,37 @@ let result = document.getElementById('res');
 let result1 = document.getElementById('output');
 let result2 = document.getElementById('output1');
 let a = 0;
-let b = 0
-let x1 = 490
-let rect1X = 0
+let b = 0;
+
+const left_border = 6
+const Right_border = 810
+const Hieght_border = 70
+const Down_border = 574
+//правый верхний угол дива
+// let divx1  = 810
+// const divy1 = 70
+// //правый нижний угол(x - не изм)
+// const divy2 = 460
+// //левый нижний угол(y = y2)
+// const divx3 = 6
+// //левый верхний угол(x - не изм)
+// const divy = 70
+
+function get_element() {
+    let element = null
+    if (b === 1){
+        element = document.getElementById("rect1");
+    }
+    else if(b === 2){
+        element = document.getElementById("rect2");
+    }
+    else if(b === 3){
+        element = document.getElementById("rect3");
+    }
+
+    return element;
+}
+
 function ch1(){
     a = +a + 1;
     result.innerText = a;
@@ -65,86 +93,103 @@ function ch8(){
 
 
 
-function moveRight1() {
-    let element = document.getElementById("rect1");
+function moveRightRect(element) {
+//     let rect=element.getBoundingClientRect();
+//     let currentRight  = rect.right;
+//     console.log(currentRight)
+//     if (currentRight  >= Right_border) {
+//         element.style.right = Right_border + "px";
+//     } else {
+//         element.style.right = (currentRight ) + "px";
+//     }
+// }
+    let currentRight= parseInt(window.getComputedStyle(element).right, 10) || 0;
+    console.log(currentRight)
+    if (currentRight +a >= Right_border) {
+        element.style.right = Right_border + "px";
+    } else {
+        element.style.right = (Right_border + a) + "px";
+    }
+}
+
+
+function moveLeftRect(element) {
+//     let rect=element.getBoundingClientRect();
+//     let currentLeft  = rect.left;
+//     console.log(currentLeft)
+//     if (currentLeft  >= left_border) {
+//         element.style.left = left_border + "px";
+//     } else {
+//         element.style.left = (currentLeft ) + "px";
+//     }
+// }
     let currentLeft= parseInt(window.getComputedStyle(element).left, 10) || 0;
-    element.style.left = (currentLeft + a) + "px";
-}
-function moveRight2() {
-    let element = document.getElementById("rect2");
-    let currentLeft= parseInt(window.getComputedStyle(element).left, 10) || 0;
-    element.style.left = (currentLeft + a) + "px";
-}
-function moveRight3() {
-    let element = document.getElementById("rect3");
-    let currentLeft= parseInt(window.getComputedStyle(element).left, 10) || 0;
-    element.style.left = (currentLeft + a) + "px";
+    if (currentLeft -a >= left_border) {
+        element.style.left = left_border + "px";
+    } else {
+        element.style.left = (left_border -a) + "px";
+    }
 }
 
+function moveHeightRect(element) {
+//     let rect=element.getBoundingClientRect();
+//     let currentHeight= rect. height;
+//     console.log(currentHeight)
+//     if (currentHeight  >= Hieght_border) {
+//         element.style.left = Hieght_border + "px";
+//     } else {
+//         element.style.top = (currentHeight ) + "px";
+//     }
+// }
+    let currentHeight= parseInt(window.getComputedStyle(element).height, 10) || 0;
+    if (currentHeight-a >= Hieght_border) {
+        element.style.height = Hieght_border + "px";
+    } else {
+        element.style.height = (Hieght_border -a) + "px";
+    }
+}
+function moveDownRect(element) {
+//     let rect=element.getBoundingClientRect();
+//     let currentDown= rect. bottom;
+//     console.log(currentDown)
+//     if (currentDown  >= Hieght_border) {
+//         element.style.left = Hieght_border + "px";
+//     } else {
+//         element.style.top = (currentDown ) + "px";
+//     }
+// }
 
-function moveLeft1() {
-    let element = document.getElementById("rect1");
-    let currentLeft= parseInt(window.getComputedStyle(element).left, 10) || 0;
-    element.style.left = (currentLeft - a) + "px";
+let currentDown= parseInt(window.getComputedStyle(element).top, 10) || 0;
+    if (currentDown +a >=Down_border) {
+        element.style.bottom = Down_border + "px";
+    } else {
+        element.style.bottom = (Down_border +a) + "px";
+    }
 }
-function moveLeft2() {
-    let element = document.getElementById("rect2");
-    let currentLeft= parseInt(window.getComputedStyle(element).left, 10) || 0;
-    element.style.left = (currentLeft - a) + "px";
-}
-function moveLeft3() {
-    let element = document.getElementById("rect3");
-    let currentLeft= parseInt(window.getComputedStyle(element).left, 10) || 0;
-    element.style.left = (currentLeft - a) + "px";
-}
-
-function moveHeight1() {
-    let element = document.getElementById("rect1");
-    let currentTop = parseInt(window.getComputedStyle(element).top, 10) || 0;
-    element.style.top = (currentTop - a) + "px";
-}
-function moveHeight2() {
-    let element = document.getElementById("rect2");
-    let currentTop = parseInt(window.getComputedStyle(element).top, 10) || 0;
-    element.style.top = (currentTop - a) + "px";
-}
-function moveHeight3() {
-    let element = document.getElementById("rect3");
-    let currentTop = parseInt(window.getComputedStyle(element).top, 10) || 0;
-    element.style.top = (currentTop - a) + "px";
-}
-
-function moveDown1() {
-    let element = document.getElementById("rect1");
-    let currentTop = parseInt(window.getComputedStyle(element).top, 10) || 0;
-
-  element.style.top =  (currentTop + a) + "px";
-}
-function moveDown2() {
-    let element = document.getElementById("rect2");
-    let currentTop = parseInt(window.getComputedStyle(element).top, 10) || 0;
-
-    element.style.top =  (currentTop + a) + "px";
-}
-function moveDown3() {
-    let element = document.getElementById("rect3");
-    let currentTop = parseInt(window.getComputedStyle(element).top, 10) || 0;
-
-    element.style.top =  (currentTop + a) + "px";
-}
+// function moveDown3() {
+//     let element = document.getElementById("rect3");
+//     let currentTop = parseInt(window.getComputedStyle(element).top, 10) || 0;
+//
+//     element.style.top =  (currentTop + a) + "px";
+// }
 function moveDown(){
-    if (b === 1){
-        moveDown1();
-        getPoint3();
+    let element = get_element()
+    if (element ){
+
+        moveDownRect(element);
+        getPoint();
+        checkDown(element)
     }
-    else if(b === 2){
-        moveDown2()
-        getPoint4()
+    else if(element){
+        moveDownRect(element)
+        getPoint1()
+        checkDown(element)
 
     }
-    else if(b === 3){
-        moveDown3();
-        getPoint5();
+    else if(element){
+        moveDownRect(element);
+        getPoint2();
+        checkDown(element)
     }
     else{
         GetControlValue();
@@ -152,27 +197,31 @@ function moveDown(){
 
 }
 function moveHeight(){
-    if (b === 1){
-        moveHeight1();
-        getPoint3();
-    }
-
-    else if(b === 2){
-        moveHeight2()
-        getPoint4()
+    let element = get_element()
+    if (element){
+        moveHeightRect(element);
+        getPoint();
+        checkHieght(element);
 
     }
-    else if(b === 3){
-        moveHeight3();
-        getPoint5();
+
+    else if(element){
+
+        moveHeightRect(element)
+        getPoint1()
+        checkHieght(element);
+    }
+    else if(element){
+        moveHeightRect(element);
+        getPoint2();
+        checkHieght(element);
+
     }
     else{
         GetControlValue();
     }
 
 }
-
-
 
 function changeBorderColor1() {
     b = 1
@@ -238,45 +287,29 @@ function GetControlValue()
 {
     alert(document.all.txtUser.Value);
 }
+
 function moveRight(){
-    if (b === 1){
-        moveRight1();
+    let element = get_element()
+    if (element) {
+        moveRightRect(element);
         getPoint();
-        if(x1<rect1X){
-            REd1();
-        }
-    }
-    else if(b === 2){
-        moveRight2()
-        getPoint1()
+        CheckRight(element)
 
     }
-    else if(b === 3){
-        moveRight3();
-        getPoint2();
-    }
-    else{
+    else {
         GetControlValue();
     }
 
 }
 
-
-
 function moveLeft(){
-    if (b === 1){
-        moveLeft1();
+    let element = get_element()
+    if (element) {
+        moveLeftRect(element);
         getPoint();
+        checkLeft(element);
     }
-    else if(b === 2){
-        moveLeft2();
-        getPoint1()
-    }
-    else if(b === 3){
-        moveLeft3();
-        getPoint2();
-    }
-    else{
+    else {
         GetControlValue();
     }
 }
@@ -287,54 +320,105 @@ function extraClear(){
 }
 
 function getPoint() {
-    let rect1 = document.getElementById("rect1");
-    let rect1X = rect1.offsetLeft;
-    result1.innerText = rect1X;
+    // инфа rect1 на оси x
+    let img = document.getElementById("rect1");
+    // Получаем координаты с использованием getBoundingClientRect()
+    let rect = img.getBoundingClientRect();
+    // Координаты относительно окна браузера
+    let r1x = rect.left;
+    let r1y = rect.top;
+    // кордината нижнего левого угла
+    // let r1x2 = r1x + 150
+    // let r1y2 = r1y + 150
+    // //координата верхнего левого угла(y - не изм)
+    // let r1x3 = r1x + 150
+    // //координата правого нижнего угал(x - не изм)
+    // let r1y4 = r1y + 150
+    // // Выводим координаты на страницу
+    document.getElementById("info1").innerText = ` X = ${r1x}, Y = ${r1y}`;
 
 }
 
 function getPoint1() {
-    let rect2 = document.getElementById("rect2");
-    let rect2X = rect2.offsetLeft;
-    result1.innerText = rect2X;
+    // инфа rect1 на оси
+    let img = document.getElementById("rect2");
+    // Получаем координаты с использованием getBoundingClientRect()
+    let rect = img.getBoundingClientRect();
+    // Координаты относительно окна браузера
+    let r2x = rect.left;
+    let r2y = rect.top;
+    // кордината нижнего левого угла
+    // let r2x2 = r2x + 150
+    // let r2y2 = r2y + 150
+    // //координата верхнего левого угла(y - не изм)
+    // let r2x3 = r2x + 150
+    // //координата правого нижнего угал(x - не изм)
+    // let r2y4 = r2y + 150
+    // // Выводим координаты на страницу
+    // // Выводим координаты на страницу
+    document.getElementById("info2").innerText = ` X = ${r2x}, Y = ${r2y}`;
 
 }
 function getPoint2() {
-    let rect3 = document.getElementById("rect3");
-    let rect3X = rect3.offsetLeft
-    result1.innerText = rect3X;
+    // инфа rect1 на оси
+    let img = document.getElementById("rect3");
+    // Получаем координаты с использованием getBoundingClientRect()
+    let rect = img.getBoundingClientRect();
+    // Координаты относительно окна браузера
+    let r3x = rect.left;
+    let r3y = rect.top;
+    document.getElementById("info3").innerText = ` X = ${r3x}, Y = ${r3y}`;
 
-}
-function REd1(){
-    document.getElementById("rect1").style.backgroundColor ="blue";
-
-
-}
-function getPoint3() {
-    let rect1 = document.getElementById("rect1");
-    let rect1Y = rect1.offsetTop;
-    result2.innerText = rect1Y;
-
-}
-
-function getPoint4() {
-    let rect2 = document.getElementById("rect2");
-    let rect2Y = rect2.offsetTop;
-    result2.innerText = rect2Y;
-
-}
-function getPoint5() {
-    let rect3 = document.getElementById("rect3");
-    let rect3Y = rect3.offsetTop
-    result2.innerText = rect3Y;
-
-}
-function REd1(){
-    if (rect1X<x1){}
 
 
 }
 
+function checkDown(element) {
+    let rect = element.getBoundingClientRect();
+    // Координаты относительно окна браузера
+    let r1x = rect.bottom;
+    if(r1x === Down_border) {
+        element.classList.add("Down")
+    }
+    else{
+        element.classList.remove("Down")
+    }
+}
 
+function checkHieght(element) {
+    let rect = element.getBoundingClientRect();
+    // Координаты относительно окна браузера
+    let r1x = rect.top;
+    if(r1x === Hieght_border) {
+        element.classList.add("Hieght")
+    }
+    else{
+        element.classList.remove("Hieght")
+    }
+}
+
+function checkLeft(element) {
+    let rect = element.getBoundingClientRect();
+    // Координаты относительно окна браузера
+    let r1x = rect.left;
+    if(r1x === left_border) {
+        element.classList.add("Left")
+    }
+    else{
+        element.classList.remove("Left")
+    }
+}
+
+function CheckRight(element){
+    let rect = element.getBoundingClientRect();
+    // Координаты относительно окна браузера
+    let r1x = rect.right;
+    if(r1x === Right_border) {
+        element.classList.add("Right")
+    }
+    else {
+        element.classList.remove("Right")
+    }
+}
 
 
