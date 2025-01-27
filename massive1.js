94ac0ad4b5980b5f584a7e64134afc995b63ab66
Cadrol1213 <romanov.sh09@gmail.com>
2025-01-27 17:58:58 +0500

    массивы1задача

---
+++ massive1.js
@@ -0,0 +1,31 @@
+function addLi() {
+    let addElement = document.getElementById('add');
+    let input = document.getElementById('name')
+    let liElement = document.createElement('li');
+
+    if (input.value === '') {
+        alert('Добавь значение')
+    }
+    else {
+        liElement.innerText = input.value;
+        liElement.classList.add('green')
+        addElement.append(liElement);
+        input.value = '';
+    }
+}
+
+function addLi2() {
+    let addElement = document.getElementById('add2');
+    let input = document.getElementById('name2')
+    let liElement = document.createElement('li');
+
+    if (input.value === '') {
+        alert('Добавь значение')
+    }
+    else {
+        liElement.innerText = input.value;
+        liElement.classList.add('city')
+        addElement.append(liElement);
+        input.value = '';
+    }
+}
