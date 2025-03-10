7a29e2e48d705b5831dc91470cf791d66b3cfdc0
Cadrol1213 <romanov.sh09@gmail.com>
2025-03-10 18:02:16 +0500

    товарыновыефункции

--- Tovari.js
+++ Tovari.js
@@ -35,11 +35,11 @@ function CreateInput2(container) {
     }
 }
 function typeofcash(){
-    TypeCash = 'no-cash'
+    TypeCash = 'безналичные'
     console.log(TypeCash)
 }
 function typeofcash1(){
-    TypeCash = 'cash'
+    TypeCash = 'наличные'
     console.log(TypeCash)
 
 }
@@ -69,45 +69,77 @@ function addProduct() {
     let newDiv = document.createElement("div");
     newDiv.className = "tovar"; // добавляем класс
     newDiv.setAttribute('data-name', name); // сохраняем имя продукта для обновления
+    let translatedColor;
+
+    if (colors === "красный") {
+        translatedColor = "red";
+    }
+
+    else if (colors == "синий") {
+        translatedColor = "blue";
+    }
+    else if (colors == "розовый") {
+        translatedColor = "pink";
+    }
+
+    else if (colors == "серый") {
+        translatedColor = "darkslategrey;";
+
+    }
+
+    else if (colors == "белый") {
+        translatedColor = "wheat";
+
+    }
+    else {
+        translatedColor = "unknown color"; // Для неопознанного цвета
+    }
+    console.log(translatedColor)
 
     if (a === 1) {
         let salary = document.getElementById("salary").value;
-        priceValue1 = price - price * (salary / 100);
-        b = b +1
+        priceValue1 = price - price * salary / 100;
+        b = b + 1
         newDiv.id = 'id' + b
         newDiv.innerHTML = `
             <span class="close"><button onclick="Delete(this)">✖</button></span>
             <span class="changer"><button onclick="changer(this)">✎ </button></span>
             <span class="arrow"><button onclick="arrow1(this)">🠝</button></span>
             <span class="arrow"><button onclick="arrow2(this)">🠟</button></span>
-            <h3 class="producth3">${name}</h3>
-            <div class="product_Category">${Category}</div>
-            <div>Особенности: <div>${specifics}</div></div>
-            <div>${story}</div>
-            <div class =  "tovar-price">Цена: <span class="NewPrice">${price}</span><div>${priceValue1}</div></div>
-            <div>Количество: <span>${much.value}</span></div>
-            <div>Пункт выдачи: <span>${geo.value} </span></div>
-            <div>Тип оплаты: <span>${TypeCash1}</span> </div>
-            <div>Цвет: <span>${colors}</span></div>
+            <div style="font-size: 26px ; padding-bottom: 5px;  font-weight: bold; padding-top: 5px" >${name}</div>
+            <div style="font-style: italic ; padding-bottom: 5px">Категории:<span>${Category}</span></div>
+            <div style="font-style:italic ; padding-bottom: 5px">Особенности: <span>${specifics}</span></div>
+            <div style="padding-bottom: 5px">${story}</div>
+            <div style="padding-bottom: 5px">Пункт выдачи: <span>${geo.value} </span></div>
+            <div style="padding-bottom: 5px">Тип оплаты: <span>${TypeCash1}</span> </div>
+            <div style="padding-bottom: 5px">Цвет: <span id = "idcolors" style="color:${translatedColor} " >${colors}</span></div>
+            <div>
+            <span class =  "tovar-price">Цена: <span class="NewPrice">${price}</span><span>руб.</span></span>
+            <span>Количество: <span>${much}</span></span>
+            </div>
+            <div class = "priceValue1">${priceValue1}<span>руб.</span></div>
         `;
     }
     else {
         b = b +1
+
         newDiv.id = 'id' + b
         newDiv.innerHTML = `  
             <span class="close"><button onclick="Delete(this)">✖</button></span>
             <span class="changer"><button onclick="changer(this)">✎ </button></span>
             <span class="arrow"><button onclick="arrow1(this)">🠝</button></span>
             <span class="arrow"><button onclick="arrow2(this)">🠟</button></span>
-            <h3 class="producth3">${name}</h3>
-            <div class="product_Category">${Category}</div>
-            <div>Особенности: <div>${specifics}</div></div>
-            <div>${story}</div>
-            <div>Цена: <span>${price}</span></div>
-            <div>Количество: <span>${much}</span></div>
-             <div>Пункт выдачи: <span>${geo} </span></div>
-            <div>Тип оплаты: <span>${TypeCash1}</span> </div>
-            <div>Цвет: <span>${colors}</span></div>
+           <div style="font-size: 26px ; padding-bottom: 5px;  font-weight: bold; padding-top: 5px" >${name}</div>
+            <div  style="font-style:italic ; padding-bottom: 5px">${Category}</div>
+            <div style="font-style:italic ; padding-bottom: 5px">Особенности: <span>${specifics}</span></div>
+            <div style="padding-bottom: 5px">${story}</div>
+             <div style="padding-bottom: 5px">Пункт выдачи: <span>${geo.value} </span></div>
+            <div style="padding-bottom: 5px">Тип оплаты: <span>${TypeCash1}</span> </div>
+            <div style="padding-bottom: 5px">Цвет: <span style="color:${translatedColor} ">${colors}</span></div>
+            <div>
+            <span style=" padding-right: 40px;">Цена: <span>${price}</span><span>руб.</span></span>
+            <span>Количество: <span>${much}</span><span>руб.</span></span>
+            </div>
         `;
     }
 
@@ -119,47 +151,66 @@ function Delete(button) {
     product.remove();
 }
 
-// function changer(button) {
-//     let productDiv = button.parentElement.parentElement;
-//     let nameElement = productDiv.querySelector('.product h3');
-//     let priceElement = productDiv.querySelector('.NewPrice');
-//     let countElement = productDiv.querySelector('div:nth-last-child(1) > div');
-//     let Category = document.getElementById("Category").value;
-//     let name = document.getElementById("name").value;
-//     let Newprice = document.getElementById("price").value;
-//     let much = document.getElementById("much").value;
-//
-//     let Checkbox = document.getElementById("Specifics");
-//     let Checkbox1 = document.getElementById("Specifics1");
-//
-//     let specifics = "";
-//     if (Checkbox.checked) {
-//         specifics += "Б/У  ";
-//     }
-//     if (Checkbox1.checked) {
-//         specifics += "Хрупкий  ";
-//     }
-//
-//     let story = document.getElementById("story").value;
-//
-//     let newName = prompt("Введите новое имя продукта:")
-//
-//
-//     nameElement.textContent;
-//     let newPrice = prompt("Введите новую цену продукта:", priceElement.textContent);
-//     let newCount = prompt("Введите новое количество продукта:", countElement.textContent);
-//
-//     if (newName) {
-//         nameElement.textContent = newName;
-//     }
-//
-//     if (newPrice) {
-//
-//         priceElement.textContent = newPrice;
-//     }
-//
-//     if (newCount) {
-//         countElement.textContent = newCount;
-//     }
-// }
-// function arrow1(){}
+function changer(button) {
+    let productDiv = button.parentElement.parentElement;
+    let nameElement = productDiv.querySelector('.tovar-name');
+    let priceElement = productDiv.querySelector('.NewPrice');
+
+    let Category = document.getElementById("Category").value;
+    let name = document.getElementById("name").value;
+    let Newprice = document.getElementById("price").value;
+    let much = document.getElementById("much").value;
+
+    let Checkbox = document.getElementById("Specifics");
+    let Checkbox1 = document.getElementById("Specifics1");
+
+    let specifics = "";
+    if (Checkbox.checked) {
+        specifics += "Б/У  ";
+    }
+    if (Checkbox1.checked) {
+        specifics += "Хрупкий  ";
+    }
+
+    let story = document.getElementById("story").value;
+
+    let newName = prompt("Введите новое имя продукта:")
+
+    nameElement.textContent;
+    let newPrice = prompt("Введите новую цену продукта:", priceElement.textContent);
+    let newCount = prompt("Введите новое количество продукта:", countElement.textContent);
+
+    if (newName) {
+        nameElement.textContent = newName;
+    }
+
+    if (newPrice) {
+
+        priceElement.textContent = newPrice;
+    }
+
+    if (newCount) {
+        countElement.textContent = newCount;
+    }
+}
+function arrow1(button) {
+    let product = button.parentElement.parentElement; // получаем родительский элемент товара
+    console.log(product)
+    let prevProduct = product.previousElementSibling; // получаем предыдущий элемент (товар)
+    console.log(prevProduct)
+
+    if (prevProduct) { // если есть предыдущий элемент
+        product.parentElement.insertBefore(product, prevProduct); // перемещаем текущий товар перед предыдущим
+    }
+}
+
+function arrow2(button) {
+    let product = button.parentElement.parentElement; // получаем родительский элемент товара
+    console.log(product)
+    let nextProduct = product.nextElementSibling; // получаем следующий элемент (товар)
+    console.log(nextProduct)
+
+    if (nextProduct) { // если есть следующий элемент
+        product.parentElement.insertBefore(nextProduct, product); // перемещаем следующий товар перед текущим
+    }
+}
